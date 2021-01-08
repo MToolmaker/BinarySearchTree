02794855e0b801bdd5b3335817d2368e8d18f4d6
Ivan Yarkov <ivan.yarkov@jetbrains.com>
2021-01-08 21:43:52 +0300

    add IUnorderedSymbolTable interface

---
+++ SymbolTable/IUnorderedSymbolTable.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace BinarySearchTree.SymbolTable
+{
+    [PublicAPI]
+    public interface IUnorderedSymbolTable<TKey, TValue> 
+        where TKey : struct, IEquatable<TKey>
+    {
+        void Put(TKey key, [NotNull] TValue value);
+        
+        bool TryGet(TKey key, [CanBeNull] out TValue value);
+
+        void Delete(TKey key);
+
+        bool Contains(TKey key);
+
+        bool IsEmpty();
+
+        int Size();
+        
+        [NotNull]
+        IEnumerable<TKey> Keys();
+        
+        [NotNull, ItemNotNull]
+        IEnumerable<TValue> Values();
+    }
+}

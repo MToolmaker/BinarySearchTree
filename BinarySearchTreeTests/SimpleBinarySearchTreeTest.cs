57973bc0e8fcbbbe4bd54aaab5e02622bab5f046
Ivan Yarkov <ivan.yarkov@jetbrains.com>
2021-01-09 12:39:16 +0300

    create NUnit tests project

---
+++ BinarySearchTreeTests/SimpleBinarySearchTreeTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace BinarySearchTreeTests
+{
+    public class SimpleBinarySearchTreeTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

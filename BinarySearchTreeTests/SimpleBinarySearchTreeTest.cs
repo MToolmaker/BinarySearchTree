a9021a69561e12f384dfb7d9ba792dd6cef5b3b7
Ivan Yarkov <ivan.yarkov@jetbrains.com>
2021-01-09 13:36:33 +0300

    add test for SimpleBinarySearchTree

--- BinarySearchTreeTests/SimpleBinarySearchTreeTest.cs
+++ BinarySearchTreeTests/SimpleBinarySearchTreeTest.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using BinarySearchTree.SymbolTables;
 using NUnit.Framework;
 
 namespace BinarySearchTreeTests
@@ -10,9 +12,26 @@ namespace BinarySearchTreeTests
         }
 
         [Test]
-        public void Test1()
+        public void SimpleTest()
         {
-            Assert.Pass();
+            var numberToString = FillSimpleBinarySearchTreeToTest();
+            var expectedNumberToString = FillExpectedMap();
+            for (var number = 1; number <= 3; number++)
+            {
+                Assert.True(expectedNumberToString.TryGetValue(number, out var expected));
+                Assert.True(numberToString.TryGet(number, out var actual));
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        private static SimpleBinarySearchTree<int, string> FillSimpleBinarySearchTreeToTest()
+        {
+            return new() {{3, "A"}, {1, "B"}, {2, "C"}};
+        }
+
+        private static Dictionary<int, string> FillExpectedMap()
+        {
+            return new() {{3, "A"}, {1, "B"}, {2, "C"}};
         }
     }
 }
